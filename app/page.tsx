50c45cbb297f5444f515b73f90198e7e43b7cec6
nurmohammadapu <mdapu2003@gmail.com>
2024-10-11 00:23:19 +0600

    Remove unnecessary app/page.tsx file

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>Welcome to My Yoom Project!</h1>
-        <p>This is my custom homepage content.</p>
-      </div>
-    );
-  }
-  
